bd6d641c182a9b7bbfc83c7b67cd6bc33b58d074
Gunnar <gunnar.montag@gmx.de>
2024-07-12 12:57:13 +0200

    resolve challenges

--- week_07/react-data-fetching/react-data-fetching_star-wars/pages/characters/[id].js
+++ week_07/react-data-fetching/react-data-fetching_star-wars/pages/characters/[id].js
@@ -7,15 +7,31 @@ export default function Character() {
   const router = useRouter();
   const { id } = router.query;
 
-  const fetcher = (...args) =>
-    fetch(...args).then((response) => response.json());
+  /*   const fetcher = (...args) =>
+    fetch(...args).then((response) => response.json()); */
+
+  const fetcher = async (url) => {
+    const res = await fetch(url);
+
+    // If the status code is not in the range 200-299,
+    // we still try to parse and throw it.
+    if (!res.ok) {
+      const error = new Error("An error occurred while fetching the data.");
+      // Attach extra info to the error object.
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
+    }
+
+    return res.json();
+  };
 
   const URL = `https://swapi.dev/api/people/${id}`;
 
   const { data, error, isLoading } = useSWR(URL, fetcher);
 
   if (error) return <p>something went wrong</p>;
-  if (isLoading) return <p>loading... please wait</p>;
+  if (isLoading) return <p>{error}</p>;
 
   console.log("data: ", data);
 
